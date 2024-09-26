57721c4498f65eb4b20a0b295cae70a985191cfd
liushengli-mac <liushengli203@163.com>
2024-09-27 00:15:36 +0800

    update: export version

--- cfgdict/__init__.py
+++ cfgdict/__init__.py
@@ -1,4 +1,5 @@
 from .config import Config, ConfigValidationError, ConfigKeyError
 from .utils import flatten_dict, unflatten_dict
+from .__version__ import __version__
 
-__all__ = ['Config', 'ConfigValidationError', 'ConfigKeyError', 'flatten_dict', 'unflatten_dict']
+__all__ = ['Config', 'ConfigValidationError', 'ConfigKeyError', 'flatten_dict', 'unflatten_dict', '__version__']
