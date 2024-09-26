9c6e0a6b0469d53c5d461c15f6542c724cf3168f
liushengli-mac <liushengli203@163.com>
2024-09-26 11:53:51 +0800

    release: v1.0.0

---
+++ examples/demo_flatten_dict.py
@@ -0,0 +1,38 @@
+from cfgdict import flatten_dict, unflatten_dict
+
+# test nested dict
+nested_dict = {
+    'a': 1,
+    'b': {
+        'c': 2,
+        'd': {
+            'e': 3
+        }
+    },
+    'f': 4
+}
+
+print(f'nested_dict: {nested_dict}')
+
+# test flatten_dict
+flattened = flatten_dict(nested_dict)
+print(f'flattened: {flattened}')
+
+assert flattened == {
+    'a': 1,
+    'b.c': 2,
+    'b.d.e': 3,
+    'f': 4
+}
+
+unflattened = unflatten_dict(flattened)
+assert unflattened == nested_dict
+
+# 测试空字典 | test empty dict
+assert flatten_dict({}) == {}
+assert unflatten_dict({}) == {}
+
+# 测试只有一层的字典 | test simple dict
+simple_dict = {'x': 1, 'y': 2, 'z': 3}
+assert flatten_dict(simple_dict) == simple_dict
+assert unflatten_dict(simple_dict) == simple_dict
---
+++ examples/demo_use_config.py
@@ -0,0 +1,14 @@
+from cfgdict import Config
+
+config_schema = [
+    dict(field='n_step', required=True, default=3, rules=dict(type='int', gt=0)),
+    dict(field='learning_rate', required=True, default=0.1, rules=dict(type='float', gt=0, max=1)),
+    dict(field='nest.gamma', required=True, default=0.99, rule=dict(type='float', min=0, max=1)),
+    dict(field='nest.epsilon', required=True, default=0.1, rules=dict(type='float', min=0, max=1)),
+    dict(field='nest.verbose_freq', required=True, default=10, rules=dict(type='int', gt=0)),
+]
+cfg_dict = {'n_step': 3, 'learning_rate': 0.1, 'nest': {'gamma': 0.99, 'epsilon': 0.1, 'verbose_freq': 10}}
+config = Config.from_dict(cfg_dict, schema=config_schema, strict=True)
+
+print(config.to_dict())
+assert config.to_dict() == {'n_step': 3, 'learning_rate': 0.1, 'nest': {'gamma': 0.99, 'epsilon': 0.1, 'verbose_freq': 10}}
